b28b2122a17b00aefd3f6cddf07b93f048851151
Seth Seeman <sseeman@pdx.edu>
2020-07-29 19:37:39 -0700

    created multid array with default values

--- minesweeper/src/main/java/edu/pdx/cs410J/sseeman/MineSweeper.java
+++ minesweeper/src/main/java/edu/pdx/cs410J/sseeman/MineSweeper.java
@@ -7,7 +7,20 @@ package edu.pdx.cs410J.sseeman;
  * class (and its tests).
  */
 public class MineSweeper {
-                                                                                    
+
+  private Character[][] map;
+
+  public MineSweeper (int n, int m) {
+    map = new Character[n][m];
+    for (int i=0; i < n; i++) {
+      for (int j=0; j < n; j++) {
+        map[i][j] = '.';
+      }
+    }
+  }
+  public Character[][] getMap() {
+    return map;
+  }
 
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
--- minesweeper/src/test/java/edu/pdx/cs410J/sseeman/MineSweeperTest.java
+++ minesweeper/src/test/java/edu/pdx/cs410J/sseeman/MineSweeperTest.java
@@ -2,12 +2,24 @@ package edu.pdx.cs410J.sseeman;
 
 import org.junit.Test;
 
+import java.io.CharConversionException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
 public class MineSweeperTest
 {
 
   @Test
-  public void canInstantiateKataClass() {
-    new MineSweeper();
+  public void canInstantiateMineSweeperClass() {
+   MineSweeper game = new MineSweeper(4, 4);
   }
 
+  @Test
+  public void canInstantiateMineSweeperDefault() {
+    MineSweeper game = new MineSweeper(4, 4);
+    Character[][] map = game.getMap();
+    assertThat(map[1][1], equalTo('.'));
+  }
 }
